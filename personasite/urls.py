98b574a7517505e3719364341813feccef3f4466
Gabriel Batista dos Santos <86369237+Gabri3lBatista@users.noreply.github.com>
2024-09-24 14:11:46 -0500

    alterações

--- personasite/urls.py
+++ personasite/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("users.urls")),  # Usuários acessam pela URL base
-    path("persona/", include("persona.urls")),  # Acesso ao persona será por /persona/
+    path("", include("persona.urls")),  # Acesso ao persona será por /persona/
     path("__debug__/", include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
 ]
