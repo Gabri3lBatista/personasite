c505086324da3668813aac1d31074995860f606f
Gabriel Batista dos Santos <86369237+Gabri3lBatista@users.noreply.github.com>
2024-10-07 12:31:21 -0500

    mudanças

--- persona/urls.py
+++ persona/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     path('problem_solutions/<int:pk>/', problema_solucoes, name='problem_solutions'),
 
 
-]
-
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
